c6b9ad8dc459d444f428c5c0edfccd4884ca86a3
Rajatz <Rajatz@gmail.com>
2023-05-03 21:49:26 +0530

    dd

--- first.tf
+++ first.tf
@@ -1,3 +1,9 @@
 provider "aws" {
-  region = ap-south-1 
+  region = "ap-south-1"
+  access_key = "AKIA2NRR6VMTI67EBENT"
+  secret_key = "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"
+}
+
+resource "aws_iam_user" "myuser" {
+  name = "dhoni"
 }
