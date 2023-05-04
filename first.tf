b2ba50a17c870adc34e89e3edd2aa2220a98332b
Rajatz <Rajatz@gmail.com>
2023-05-04 21:09:25 +0530

    today

--- first.tf
+++ first.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/* provider "aws" {
   region = "ap-south-1"
   access_key = "AKIA2NRR6VMTI67EBENT"
   secret_key = "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"
@@ -31,4 +31,4 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
-}
+} */
