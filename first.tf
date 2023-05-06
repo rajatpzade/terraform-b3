3b04018de795ee48f945d58fd5b3e34b87323228
Rajat Zade <93313018+rajatpzade@users.noreply.github.com>
2023-05-06 16:30:49 +0530

    Update first.tf
    second comiit

--- first.tf
+++ first.tf
@@ -1,7 +1,7 @@
 /* provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA2NRR6VMTI67EBENT"
-  secret_key = "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"
+  access_key = "#"
+  secret_key = "#"
 }
 
 /* resource "aws_iam_user" "myuser" {
